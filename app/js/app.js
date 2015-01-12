2545dc138c9c7006fbe5bb70bedeefdd3ceda130
Dave Lunny <dlunny@featurecreeptechnology.com>
2015-01-11 22:44:50 -0500

    resume routing+; nav+;
    nav added to main screen and routing is in place for resume section

--- app/js/app.js
+++ app/js/app.js
@@ -13,27 +13,54 @@ app.config(function($stateProvider, $urlRouterProvider) {
         url: "/",
         templateUrl: "partials/main.html",
         controller: "Porfolio"
+      })
+      .state('resume', {
+        url: '/resume',
+        templateUrl: 'partials/resume.html',
+        controller: "Resume"
       });
+
 });
 
 
 
 //	MAIN CONTROLLER
-app.controller('Porfolio', function ($scope, $log) {
+app.controller('Porfolio', function ($scope, $log, $state) {
 
    var l = document.getElementById('logo'),
       tl = new TimelineMax(),
       icons = document.getElementsByClassName('icon');
 
 
-      tl.to(l, 1.75, {opacity:1, ease:Power3.easeOut});
-      tl.to(icons, 0.55, {opacity: 1, margin: 0, ease:Power2.easeIn});
-      tl.to(icons[0], 0.45, {top: "-15%", margin: "0 0 0 -7.5px", delay: -0.1, ease: Elastic.easeOut});
-      tl.to(icons[1], 0.45, {top: "69%", left: "5%", delay: -0.45, ease: Elastic.easeOut});
-      tl.to(icons[2], 0.45, {top: "69%", left: "84%", delay: -0.45, ease: Elastic.easeOut});
+      tl.to(l, 2, {opacity:1, delay: 1, ease:Power3.easeOut});
+      tl.to(icons, 0.25, {opacity: 1, margin: 0, ease:Power2.easeIn});
+      tl.to(icons[0], 0.4, {top: "-5%", left: "50%", margin: "0 0 0 -20px", delay: -0.25, ease: Elastic.easeOut});
+      tl.to(icons[1], 0.4, {top: "83%", left: "5%", delay: -0.4, ease: Elastic.easeOut});
+      tl.to(icons[2], 0.4, {top: "83%", left: "82%", delay: -0.4, ease: Elastic.easeOut});
+
+
+  $scope.goResume = function(){
+    $state.go('resume');
+  };
+
+  $scope.goContact = function(){
+    alert('Go Contact');
+  };
+
 
 
 });
+
+
+app.controller('Resume', function ($scope, $log, $state) {
+
+
+
+
+
+});
+
+
 
 
 
