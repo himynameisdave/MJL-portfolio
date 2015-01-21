4519e933aae67908ed47f91e33a33b4e6833267d
Dave Lunny <dlunny@featurecreeptechnology.com>
2015-01-21 16:46:55 -0500

    all the rest of that

--- app/js/app.js
+++ app/js/app.js
@@ -1,11 +1,35 @@
 //	DEFINE APP
-var app = angular.module('app', []);
+var app = angular.module('app', ['ui.router']);
+
+app.config(["$stateProvider", "$urlRouterProvider", function($stateProvider, $urlRouterProvider) {
+
+  $urlRouterProvider.otherwise("/");
+
+  /*
+  $stateProvider
+      .state("intro", {
+        url: "/",
+        templateUrl: "partials/intro.html"
+      })
+      .state("what-is-yeoman", {
+        url: "/what-is-yeoman",
+        templateUrl: "partials/what-is-yeoman.html"
+      })
+      .state("installation", {
+        url: "/installation",
+        templateUrl: "partials/installation.html"
+      });
+  */
+
+}]);
+
 
 //	MAIN CONTROLLER
-app.controller('Controller', function ($scope) {
+app.controller('Main', ["$scope", "$state", function ($scope, $state) {
+
+  // $scope.isHome = true;
 
 
-	//	Make unicorns fly...
+  $scope.isHome = false;
 
-
-});
+}]);
