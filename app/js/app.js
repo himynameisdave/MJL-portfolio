5d65436d59cf617a61341328ecdf225d984d854d
Dave Lunny <dlunny@featurecreeptechnology.com>
2015-01-22 23:57:46 -0500

    adds scrollstay functionality

--- app/js/app.js
+++ app/js/app.js
@@ -27,9 +27,95 @@ app.config(["$stateProvider", "$urlRouterProvider", function($stateProvider, $ur
 //	MAIN CONTROLLER
 app.controller('Main', ["$scope", "$state", function ($scope, $state) {
 
-  // $scope.isHome = true;
+  //  get to the top right quick
+  runTheJewels(0.001, 0);
 
-
-  $scope.isHome = false;
+  setupScrollListener();
 
 }]);
+
+
+var setupScrollListener = function(){
+
+  var doneScrollAbout = false;
+
+  window.onscroll = function(e){
+
+    //throttler
+    if( Math.round(window.scrollY) % 5 === 0 ){
+
+      var sections = [{
+                        id: 'logo'
+                      },{
+                        id: 'print'
+                      },{
+                        id: 'web'
+                      }];
+
+      //  get position of all of our sections
+      sections.forEach(function(val, i){
+        //  grab the element itself (?)
+        var el   = document.getElementById(val.id);
+        //  if the elements actually exist & the 
+        if(el !== null){
+          //  save some data on our position
+          var pos             = sections[i].position = el.getBoundingClientRect(),
+              elTop           = sections[i].position.top,
+              elHeight        = sections[i].position.height,
+              posInDoc        = sections[i].posInDoc = elTop + document.body.scrollTop,
+              percentScrolled = Math.round(elTop / elHeight * 100);
+
+            if( elTop > 0 && !doneScrollAbout){
+              doneScrollAbout = true;
+
+              console.log("==="+sections[i].id+"===");
+
+              if( percentScrolled <= 49 ){
+                setTimeout(function(){
+                  //if not the last one
+                  if(i !== sections.length){
+                    console.log('scrolling down');
+                    runTheJewels(1.5, sections[i].posInDoc);
+                  }else{
+                    console.log('scrolling down');
+                    runTheJewels(1.5, posInDoc);
+                  }
+                  doneScrollAbout = false;
+                }, 1250);
+              }
+
+              if( percentScrolled > 50 ){
+                setTimeout(function(){
+                  if(i !== 0){
+                    console.log('scrolling back up');
+                    runTheJewels(1.5, sections[i-1].posInDoc);
+                     doneScrollAbout = false;
+                  }else{
+                    var homePos = document.getElementById('home').getBoundingClientRect().top + document.body.scrollTop;
+                    runTheJewels(1.5, homePos);
+                    doneScrollAbout = false;
+                  }
+                }, 1250);
+              }
+
+            }
+
+        }//end isNull check
+      });
+
+    }//end throttler
+
+  };
+
+};
+
+
+
+//  t=time; d=distance;
+var runTheJewels = function(t, d){
+  TweenLite.to(window, t, {scrollTo:{y:d}, ease:Power4.easeOut});
+},
+scrollUp = function( oldPos  ){
+
+};
+
